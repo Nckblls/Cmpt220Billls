58ebd3ff7a61da94b280300c305fb9a52ad2f727
Nicholas Bills <Nicholas.bills1@marist.edu>
2017-04-28 20:54:28 -0400

    Long msg.

--- lab/7/Problem1017.java
+++ lab/7/Problem1017.java
@@ -1,5 +1,16 @@
+import java.lang.Math;
+import java.math.BigDecimal;
 public class Problem1017{
   public static void main(String[] args){
+    
+    BigDecimal x = new BigDecimal((long)Math.sqrt(Long.MAX_VALUE));
+    for (int i = 1; i < 11; i++){
+      x = x.add(BigDecimal.ONE);
+      BigDecimal x2 = x.multiply(x);
+      System.out.println(i + " "+"Number:" + x + ", Square: " + x2);
+    }
+    
+    
     
   }
 
